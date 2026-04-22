41efb230e7918cb4718f7585f6f4fb82a38f6675
Alvarosky <ajlondonom@unal.edu.co>
2026-04-22 12:01:07 -0500

    jjj

--- apps/api/drizzle/0001_add_ajuste_enum.sql
+++ apps/api/drizzle/0001_add_ajuste_enum.sql
@@ -1,2 +1,15 @@
-ALTER TYPE "public"."movement_type" ADD VALUE 'AJUSTE';
-ALTER TYPE "public"."movement_type" ADD VALUE 'TRASLADO';
+-- Añadir AJUSTE si no existe
+DO $$ 
+BEGIN 
+  IF NOT EXISTS (SELECT 1 FROM pg_type t JOIN pg_enum e ON t.oid = e.enumtypid WHERE t.typname = 'movement_type' AND e.enumlabel = 'AJUSTE') THEN 
+    ALTER TYPE "public"."movement_type" ADD VALUE 'AJUSTE'; 
+  END IF; 
+END $$;
+
+-- Añadir TRASLADO si no existe
+DO $$ 
+BEGIN 
+  IF NOT EXISTS (SELECT 1 FROM pg_type t JOIN pg_enum e ON t.oid = e.enumtypid WHERE t.typname = 'movement_type' AND e.enumlabel = 'TRASLADO') THEN 
+    ALTER TYPE "public"."movement_type" ADD VALUE 'TRASLADO'; 
+  END IF; 
+END $$;
